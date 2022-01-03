b2e9b85639a3c5a20c8b760255f8c26cc5be2db7
Praveen Sana <spraveen0244@gmail.com>
2022-01-03 21:31:27 +0530

    refactor(db configuration): added configuration annotation for config file

--- src/main/java/com/astra/demo/AstraJavaAppApplication.java
+++ src/main/java/com/astra/demo/AstraJavaAppApplication.java
@@ -1,20 +1,13 @@
 package com.astra.demo;
 
 import com.astra.demo.config.AstraDBConfig;
-import com.datastax.oss.driver.api.core.CqlSessionBuilder;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
-
-import java.nio.file.Path;
 
 @SpringBootApplication
-// @Import(AstraDBConfig.class)
 @EnableConfigurationProperties(AstraDBConfig.class)
 public class AstraJavaAppApplication {
 
--- src/main/java/com/astra/demo/config/AstraDBConfig.java
+++ src/main/java/com/astra/demo/config/AstraDBConfig.java
@@ -2,9 +2,12 @@ package com.astra.demo.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
 import java.io.File;
 
 @Data
+@Configuration
 @ConfigurationProperties(prefix = "datastax.astra")
 public class AstraDBConfig {
 
